cc996536f4e8231ba84bbf85f0ca3a0a3c62d5f3
Gabi <gabyiy2000@hotmail.com>
2024-01-19 18:34:10 +0100

    next

---
+++ controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+
+class LoginController{
+
+    public static function login(Router $router){
+
+    echo "desde el login";
+    }
+
+    public static function logout (){
+
+        echo "desde el logout";
+    }
+
+    public function olvide(){
+
+    }
+    public function recuperar(){
+
+    }
+    public function crear(){
+
+    }
+
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -2,9 +2,28 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
+
+//Initiem sesiunea
+//avem un get pentru a ne duce la formular si un post cand trimitem datele din buton
+$router->get("/",[LoginController::class,"login"]);
+$router->post("/",[LoginController::class,"login"]);
+
+$router->get("/logout",[LoginController::class,"logout"]);
+
+//Recupereaza parola
+$router->get("/olvide",LoginController::class,"olvide");
+$router->post("/olvide",LoginController::class,"olvide");
+$router->get("/recuperar",LoginController::class,"recuperar");
+$router->post("/recuperar",LoginController::class,"recuperar");
+
+//Creem conturi
+
+$router->get("/crear-cuenta",LoginController::class,"crear");
+$router->post("/crear-cuenta",LoginController::class,"crear");
 
 
 
